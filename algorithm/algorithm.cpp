f43b2099982ea6f1f7d5665f60b0501f150aee6d
Milan Oberkirch <milan@oberkirch.org>
2013-09-10 22:11:02 +0200

    Refactored according to the google-styleguide.

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -1,60 +1,59 @@
+// Copyright (2013) Milan Oberkirch <milan@oberkirch.org>.
+
 #include <Python.h>
 #include <algorithm>
 #include "PythonSequenceIterator.cpp"
 
 static PyObject * algorithm_binary_search(PyObject *self, PyObject *args) {
-  PyObject *list = NULL;
-  PyObject *value = NULL;
-  if (PyArg_ParseTuple(args, "OO", &list, &value) and PySequence_Check(list)) {
-    assert(PySequence_Size(list));
-    PythonSequenceIterator first(list, 0);
-    PythonSequenceIterator last(list, PySequence_Size(list));
-    bool exists = std::binary_search(first, last, value);
-    if (exists)
-      Py_RETURN_TRUE;
-    else
-      Py_RETURN_FALSE;
-  } else {
-    return NULL;
-  }
+    PyObject *list = NULL;
+    PyObject *value = NULL;
+    if (PyArg_ParseTuple(args, "OO", &list, &value) && PySequence_Check(list)) {
+        assert(PySequence_Size(list));
+        PythonSequenceIterator first(list, 0);
+        PythonSequenceIterator last(list, PySequence_Size(list));
+        bool exists = std::binary_search(first, last, value);
+        if (exists)
+            Py_RETURN_TRUE;
+        else
+            Py_RETURN_FALSE;
+    } else {
+        return NULL;
+    }
 }
 
 static PyMethodDef AlgorithmMethods[] = {
     {"binary_search",  algorithm_binary_search, METH_VARARGS,
-     "Perform a binary_search using the C++-implementation from stl."},
+        "Perform a binary_search using the C++-implementation from stl."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
 static struct PyModuleDef algorithmmodule = {
-   PyModuleDef_HEAD_INIT,
-   "algorithm",   /* name of module */
-   "Provides functions (currently only binary_search) known from C++ stl/<algorithm>.", /* module documentation, may be NULL */
-   -1,       /* size of per-interpreter state of the module,
-                or -1 if the module keeps state in global variables. */
-   AlgorithmMethods
+    PyModuleDef_HEAD_INIT,
+    "algorithm",
+    "Provides binary_search known from C++ stl/<algorithm>.",
+    -1,
+    AlgorithmMethods
 };
 
-PyMODINIT_FUNC PyInit_algorithm(void)
-{
+PyMODINIT_FUNC PyInit_algorithm(void) {
     return PyModule_Create(&algorithmmodule);
 }
 
-int
-main(int argc, char *argv[])
-{
-  wchar_t * wargv;
-  mbstowcs(wargv, argv[0], 100);
-  /* Add a built-in module, before Py_Initialize */
-  PyImport_AppendInittab("algorithm", PyInit_algorithm);
+    int
+main(int argc, char *argv[]) {
+    wchar_t * wargv = NULL;
+    mbstowcs(wargv, argv[0], 100);
+    /* Add a built-in module, before Py_Initialize */
+    PyImport_AppendInittab("algorithm", PyInit_algorithm);
 
-  /* Pass argv[0] to the Python interpreter */
-  Py_SetProgramName(wargv);
+    /* Pass argv[0] to the Python interpreter */
+    Py_SetProgramName(wargv);
 
-  /* Initialize the Python interpreter.  Required. */
-  Py_Initialize();
+    /* Initialize the Python interpreter.  Required. */
+    Py_Initialize();
 
-  /* Optionally import the module; alternatively,
-     import can be deferred until the embedded script
-     imports it. */
-  PyImport_ImportModule("algorithm");
+    /* Optionally import the module; alternatively,
+       import can be deferred until the embedded script
+       imports it. */
+    PyImport_ImportModule("algorithm");
 }
